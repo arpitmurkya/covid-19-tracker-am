b42ad2a02159e8cba706b5a181f9c1301e883ffc
Arpit Murkya <arpitmurkya@gmail.com>
2020-04-26 04:44:52 +0530

    Resolving issue with node version

---
+++ build/precache-manifest.1a9e8736aa420c99627f0e7e8b6fa758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b6d72892e9c5323668c2ed874bcbb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b2424929ee1a427d07a",
+    "url": "/static/css/main.6eca9097.chunk.css"
+  },
+  {
+    "revision": "2426f6a1fa6fea1e961d",
+    "url": "/static/js/2.7d7fead3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.7d7fead3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2424929ee1a427d07a",
+    "url": "/static/js/main.ea214a4b.chunk.js"
+  },
+  {
+    "revision": "a0ff94ea0192e7754a94",
+    "url": "/static/js/runtime-main.77c08164.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
